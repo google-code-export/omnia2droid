1ab82ab99cede213a09e3fa6d1ba5e14e543136e
elbee1972@gmail.com <elbee1972@gmail.com@dcf86dc9-998a-afa3-c1f0-cc4217cd85a6>
2011-03-15 22:27:23 +0000

    b7610: fixed non-working POWER key

--- trunk/kern_oII/drivers/input/keyboard/s3c-keypad.c
+++ trunk/kern_oII/drivers/input/keyboard/s3c-keypad.c
@@ -208,12 +208,22 @@ static irqreturn_t s3c_keygpio_isr(int irq, void *dev_id)
 	// upside/down.
 	if (in_sleep) {
 		if (key_status == prev_key_status) {
+#ifdef PHONE_I8000
 			INPUT_REPORT_KEY(dev, 26, key_status ? 1 : 0); 
+#endif
+#ifdef PHONE_B7610
+			INPUT_REPORT_KEY(dev, KEY_POWER, key_status ? 1 : 0); 
+#endif
 		}
 		in_sleep = 0;
 	}
 
-	INPUT_REPORT_KEY(dev, 26, key_status ? 0 : 1);
+#ifdef PHONE_I8000
+	INPUT_REPORT_KEY(dev, 26, key_status ? 0 : 1); 
+#endif
+#ifdef PHONE_B7610
+	INPUT_REPORT_KEY(dev, KEY_POWER, key_status ? 0 : 1); 
+#endif
 
 	prev_key_status = key_status;
 	//printk(KERN_DEBUG "s3c_keygpio_isr pwr key_status =%d\n", key_status);
