440be240665b5ccc708e25419c87a606af8c2790
elbee1972@gmail.com <elbee1972@gmail.com@dcf86dc9-998a-afa3-c1f0-cc4217cd85a6>
2011-03-20 23:04:55 +0000

    merged till rev113 http://o2droid.phj.hu/trac/log/

---
+++ kern_oII/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 Mon Mar 21 00:04:04 CET 2011"
+#define LINUX_COMPILE_TIME "00:04:04"
+#define LINUX_COMPILE_BY "elbee"
+#define LINUX_COMPILE_HOST "laptop"
+#define LINUX_COMPILE_DOMAIN ""
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-51) "
