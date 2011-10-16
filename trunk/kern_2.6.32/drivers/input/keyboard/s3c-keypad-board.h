b4d5f9afa3e1880ee17a46d16d1d71a45cd3e973
erikcas1972@gmail.com <erikcas1972@gmail.com@dcf86dc9-998a-afa3-c1f0-cc4217cd85a6>
2011-10-16 21:59:17 +0000

    Screenrotation on keyboardslide for B7610 added

--- trunk/kern_2.6.32/drivers/input/keyboard/s3c-keypad-board.h
+++ trunk/kern_2.6.32/drivers/input/keyboard/s3c-keypad-board.h
@@ -18,32 +18,38 @@
 #define KEYPAD_COLUMNS  (3)     /* GPIO_KEYSENSE_0 ~ GPIO_KEYSENSE_2 */
 #define KEYPAD_ROWS     (3)     /* GPIO_KEYSCAN_0 ~ GPIO_KEYSCAN_2 */
 #else
-#define KEYPAD_COLUMNS  (8)     /* GPIO_KEYSENSE_0 ~ GPIO_KEYSENSE_7 */
+#define KEYPAD_COLUMNS  (7)     /* GPIO_KEYSENSE_0 ~ GPIO_KEYSENSE_6 */
 #define KEYPAD_ROWS     (7)     /* GPIO_KEYSCAN_0 ~ GPIO_KEYSCAN_6 */
 #endif
 #define MAX_KEYPAD_NR   (KEYPAD_COLUMNS * KEYPAD_ROWS)
 #define MAX_KEYMASK_NR  32
 
-#define KEYCODE_SYM	251
-#define KEYCODE_MENU	252
-#define KEYCODE_TAB	253
+#define KEYCODE_DUMPKEY 247
+#define KEYCODE_SENDEND 248
+#define KEYCODE_ENDCALL 249
+#define KEYCODE_FOCUS   250
+#define KEYCODE_HOLDKEY 251
+//#define KEYCODE_CAMERA  252
+
+#define CONFIG_INSTINCTQ_REV	0x0000
+#define GPIO_POWER_N		GPIO_nPOWER
+#define GPIO_POWER_N_AF		GPIO_nPOWER_AF
 
 u32 g_board_num = CONFIG_INSTINCTQ_REV;
 
+
 #ifndef CONFIG_MACH_MAX
-struct s3c_keypad_slide slide_instinctq = {IRQ_EINT(4), GPIO_HALL_SW, GPIO_HALL_SW_AF, 1};
+struct s3c_keypad_slide slide_instinctq = {IRQ_EINT(11), GPIO_HALL_SW, GPIO_HALL_SW_AF, 1};
 #endif
 
 struct s3c_keypad_special_key special_key_instinctq_00_01[] = {
-	{0x00000000, 0x00010000, KEYCODE_FOCUS},
-	{0x01000100, 0x00000000, KEYCODE_DUMPKEY},
+	{0x00000000, 0x00100000,  KEYCODE_FOCUS},
+	{0x20000020, 0x00000020,  KEYCODE_DUMPKEY},
 };
 
 struct s3c_keypad_special_key special_key_instinctq_01a_40[] = {
-	{0x01000100, 0x00000000, KEYCODE_DUMPKEY},
-	{0x00000000, 0x04200000, KEYCODE_SYM},
-	{0x00000000, 0x20200000, KEYCODE_MENU},
-	{0x00000000, 0x00280000, KEYCODE_TAB},
+	{0x00020000,   0x00000000,    KEYCODE_FOCUS},
+	{0x00050400,   0x00000000,    KEYCODE_DUMPKEY},
 };
 struct s3c_keypad_gpio_key gpio_key_instinctq_01[] = {
 	{IRQ_EINT(5),  GPIO_POWER_N     , GPIO_POWER_N_AF     , KEYCODE_ENDCALL, 1},
@@ -51,7 +57,7 @@ struct s3c_keypad_gpio_key gpio_key_instinctq_01[] = {
 
 #if defined(CONFIG_MACH_MAX)
 struct s3c_keypad_extra s3c_keypad_extra[] = {
-	{0x0000, NULL,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},	
+	{0x0000, NULL,  &special_key_instinctq_01a_40[0], 2,   &gpio_key_instinctq_01[0] , 1, 0},	
 };
 #elif defined(CONFIG_MACH_VITAL)
 struct s3c_keypad_extra s3c_keypad_extra[] = {
@@ -59,13 +65,8 @@ struct s3c_keypad_extra s3c_keypad_extra[] = {
 };
 #else
 struct s3c_keypad_extra s3c_keypad_extra[] = {
-	{0x0000, &slide_instinctq,  &special_key_instinctq_00_01[0], 2,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x0010, &slide_instinctq,  &special_key_instinctq_00_01[0], 2,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x001a, &slide_instinctq,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x0020, &slide_instinctq,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x0030, &slide_instinctq,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x0040, &slide_instinctq,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},
-	{0x0050, &slide_instinctq,  &special_key_instinctq_01a_40[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},	
+	{0x0000, &slide_instinctq,  &special_key_instinctq_00_01[0], 4,   &gpio_key_instinctq_01[0] , 1, 0},
+	
 };
 #endif
 
